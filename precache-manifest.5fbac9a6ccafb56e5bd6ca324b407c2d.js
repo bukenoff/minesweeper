27bd5eb6dad2690893a826f5d104a4c37286213a
serik <bukenoff.s@gmail.com>
2019-12-30 18:50:07 +0700

    deploy

---
+++ precache-manifest.5fbac9a6ccafb56e5bd6ca324b407c2d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc431826f2f479d7615",
+    "url": "/my-minesweeper-vuejs-vuex/css/app.e66942bb.css"
+  },
+  {
+    "revision": "2f02a21a34054ddba3ac1abda0e0a41d",
+    "url": "/my-minesweeper-vuejs-vuex/img/back.2f02a21a.svg"
+  },
+  {
+    "revision": "6c840a951817d63b39551f4127aefa58",
+    "url": "/my-minesweeper-vuejs-vuex/img/bomb.6c840a95.svg"
+  },
+  {
+    "revision": "96f9bc50a746f479962dc591948636e0",
+    "url": "/my-minesweeper-vuejs-vuex/img/flag.96f9bc50.svg"
+  },
+  {
+    "revision": "93e34083436770502568629778808173",
+    "url": "/my-minesweeper-vuejs-vuex/index.html"
+  },
+  {
+    "revision": "8fc431826f2f479d7615",
+    "url": "/my-minesweeper-vuejs-vuex/js/app.3c2ac1c5.js"
+  },
+  {
+    "revision": "5f220f45794754178fc9",
+    "url": "/my-minesweeper-vuejs-vuex/js/chunk-vendors.53bbd6c2.js"
+  },
+  {
+    "revision": "f95bfe8a2776b90c8237676c648d1159",
+    "url": "/my-minesweeper-vuejs-vuex/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-minesweeper-vuejs-vuex/robots.txt"
+  }
+]);
